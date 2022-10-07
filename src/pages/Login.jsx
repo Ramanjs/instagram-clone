743f3102c0c1e452b638cb54632945293dd0dbcf
Ramanjs <raman.s2662@gmail.com>
2022-10-07 08:20:10 +0530

    implement password masking

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import instagram from '../images/instagram.png';
 import { Link } from 'react-router-dom';
 
@@ -6,14 +6,25 @@ const Login = () => {
 
   const [userName, setUserName] = useState('')
   const [passwd, setPasswd] = useState('')
+  const [passwdMask, setPasswdMask] = useState('')
+
+  const handlePasswdChange = (curPasswd) => {
+    if (curPasswd.length > passwd.length) { // new character written
+      setPasswd(passwd + curPasswd.slice(passwd.length, curPasswd.length))
+    } else { // character deleted
+      setPasswd(passwd.slice(0, passwd.length - 1))
+    }
+
+    setPasswdMask('.'.repeat(curPasswd.length))
+  }
 
   return (
     <div className="flex justify-center items-center w-full min-h-screen bg-slate-100">
       <div className="flex flex-col items-center w-11/12 bg-white p-5 border-2 border-gray-200 space-y-4 shadow-lg">
         <img className="w-1/2" src={instagram} alt="instagram" />
         <form className="flex flex-col w-5/6 space-y-2">
-          <input type="text" className="w-full border-2 border-gray-200 p-2 focus:border-gray-300 focus:outline-none" placeholder="Username" onChange={e => setUserName(e.target.value)} value={userName} />
-          <input type="text" className="w-full border-2 border-gray-200 p-2 focus:border-gray-300 focus:outline-none" placeholder="Password" onChange={e => setPasswd(e.target.value)} value={passwd} />
+          <input type="text" className="w-full border-2 border-gray-200 p-2 focus:border-gray-300 focus:outline-none" placeholder="Username" onChange={e => setUserName(e.target.value)} value={userName} required />
+          <input type="text" className="w-full border-2 border-gray-200 p-2 focus:border-gray-300 focus:outline-none font-bold placeholder:font-normal" placeholder="Password" onChange={e => handlePasswdChange(e.target.value)} value={passwdMask} required />
           <input type="submit" className="w-full bg-blue-400 hover:bg-blue-500 text-white font-bold rounded py-2 cursor-pointer mt-2" value="Log In"/>
         </form>
         <p>Don't have an account? <Link to="/signup" className="text-blue-500 font-bold">Sign up</Link></p>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -12,9 +12,9 @@ const SignUp = () => {
       <div className="flex flex-col items-center w-11/12 bg-white p-5 border-2 border-gray-200 space-y-4 shadow-lg">
         <img className="w-1/2" src={instagram} alt="instagram" />
         <form className="flex flex-col w-5/6 space-y-2">
-          <input type="text" className="w-full border-2 border-gray-200 p-2 focus:border-gray-300 focus:outline-none" placeholder="Full Name" onChange={e => setFullName(e.target.value)} value={fullName} />
-          <input type="text" className="w-full border-2 border-gray-200 p-2 focus:border-gray-300 focus:outline-none" placeholder="Username" onChange={e => setUserName(e.target.value)} value={userName} />
-          <input type="text" className="w-full border-2 border-gray-200 p-2 focus:border-gray-300 focus:outline-none" placeholder="Password" onChange={e => setPasswd(e.target.value)} value={passwd} />
+          <input type="text" className="w-full border-2 border-gray-200 p-2 focus:border-gray-300 focus:outline-none" placeholder="Full Name" onChange={e => setFullName(e.target.value)} value={fullName} required/>
+          <input type="text" className="w-full border-2 border-gray-200 p-2 focus:border-gray-300 focus:outline-none" placeholder="Username" onChange={e => setUserName(e.target.value)} value={userName} required/>
+          <input type="text" className="w-full border-2 border-gray-200 p-2 focus:border-gray-300 focus:outline-none" placeholder="Password" onChange={e => setPasswd(e.target.value)} value={passwd} required/>
           <input type="submit" className="w-full bg-blue-400 hover:bg-blue-500 text-white font-bold rounded py-2 cursor-pointer mt-2" value="Sign Up"/>
         </form>
         <p>Already have an account? <Link to="/login" className="text-blue-500 font-bold">Log In</Link></p>
