a03d11fc1da506400a7a7c8462de03a1c41f5f69
Ramanjs <raman.s2662@gmail.com>
2022-11-05 22:59:17 +0530

    setup mongodb

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb+srv://ramanjs:hesoyamsa26@dev.wlk2w3u.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error: "));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
