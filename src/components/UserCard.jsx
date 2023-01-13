8bd9581f1cefac4f7f4c9ac625fabd68859a36fb
Ramanjs <raman.s2662@gmail.com>
2023-01-14 00:20:59 +0530

    fix: user card css

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const User = (props) => {
   return (
     <Link to={`/${props.user.handle}`} className="w-[45%] h-40 flex flex-col items-center justify-between m-1 p-2 border-2 rounded-md shadow-sm">
-        <img className="w-24 h-24 border-none rounded-full bg-gray-100" alt="" src={`${baseUrl}/users/images/${props.user.pfp}`}/>
+        <img className="w-24 h-24 border-none rounded-[50%] bg-gray-100 object-cover" alt="" src={`${baseUrl}/users/images/${props.user.pfp}`}/>
         <p className="text-lg text-center font-semibold">{props.user.handle}</p>
     </Link>
   )
