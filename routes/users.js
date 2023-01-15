11b9d9e771f5bbf6f0e5b2481e19c2353802b194
Ramanjs <raman.s2662@gmail.com>
2023-01-15 17:54:38 +0530

    refactor: user routes

--- routes/users.js
+++ routes/users.js
@@ -2,21 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 const userController = require('../controllers/userController');
+const editUserProfile = userController.editUserProfile
+const userDetail = userController.userDetail
+const getPosts = userController.getPosts
+const createPost = userController.createPost
+const addFollower = userController.addFollower
+const getSuggested = userController.getSuggested
+const getFeed = userController.getFeed
+
 const authController = require('../controllers/authController');
 const { upload } = require('../utils/upload')
 const { uploadImage } = require('../utils/upload')
 const verifyToken = authController.verifyToken;
 
-router.get('/:handle', userController.userDetail);
-router.post('/:handle/profile', verifyToken, upload.single('image'), uploadImage, userController.editUserProfile);
+router.get('/:handle', userDetail);
+router.post('/:handle/profile', verifyToken, upload.single('image'), uploadImage, editUserProfile);
 
-router.get('/:handle/posts', userController.getPosts);
-router.post('/:handle/posts', verifyToken, upload.single('image'), uploadImage, userController.createPost);
+router.get('/:handle/posts', getPosts);
+router.post('/:handle/posts', verifyToken, upload.single('image'), uploadImage, createPost);
 
-router.post('/:handle/followers', verifyToken, userController.addFollower);
+router.post('/:handle/followers', verifyToken, addFollower);
 
-router.get('/:handle/suggested', userController.getSuggested);
+router.get('/:handle/suggested', getSuggested);
 
-router.get('/:handle/feed', verifyToken, userController.getFeed);
+router.get('/:handle/feed', verifyToken, getFeed);
 
 module.exports = router;
